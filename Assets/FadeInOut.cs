aa5fd98f106b145ccd55ed0bddea1e9e88ff5687
qq598867756 <598867756@qq.com>
2017-12-22 15:55:52 +0800

    IE

---
+++ Assets/FadeInOut.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInOut {
+
+    public static IEnumerator FadeImage(Image target, float duration, Color color)
+    {
+        if (target == null)
+            yield break;
+        float alpha = target.color.a;
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / duration)
+        {
+            if (target == null)
+                yield break;
+            Color targetColor = new Color(color.r, color.g, color.b, Mathf.SmoothStep(alpha, color.a, t));
+            target.color = targetColor;
+            yield return null;
+        }
+        target.color = color;
+    }
+    
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour {
+    public Image a;
+    public Color A1;
+
+
+	
+	private void Start () {
+		
+	}
+	
+	
+	private void Update () {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            StartCoroutine(FadeInOut.FadeImage(a,3f,A1));
+        }
+		
+	}
+}
